ea474fbe1e1dd3b46ded8b825eadc5e1c6c414c5
Priyansh Goantiya <161962313+priyanshgoantiya@users.noreply.github.com>
2025-04-07 16:06:19 +0530

    Update app1.py

--- streamlit_app/app1.py
+++ streamlit_app/app1.py
@@ -7,7 +7,7 @@ import pickle
 import joblib
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 # Load the pickled model
-with open('/content/model(XGB).pikle', 'rb') as f:
+with open('models/model(XGB).pikle', 'rb') as f:
     pipeline = pickle.load(f)
 
 # Save using joblib and reload
